4b007b651547ed246f0e7d67d560dd10b9e050a4
Damian Wantzing <damianwantzing@gmail.com>
2024-01-17 15:25:22 +0100

    change request to interface

--- TODO.md
+++ TODO.md
@@ -8,5 +8,3 @@ e.g. router->fileserver("/static", __DIR__."/stat");
 root of /stat is uri/static
 so /static/file1 file1 will be in the /stat directory
 When no file is specified we will show the directory structure, through which a user can browse and click
-
-
--- src/Request/Request.php
+++ src/Request/Request.php
@@ -5,76 +5,23 @@ namespace Router\Request;
 use Router\Method\Method;
 use Router\Routes\Route;
 
-class Request
+interface Request
 {
-    private Method $method;
-    private array $headers;
-    private string $body;
-    private Route $route;
-    private string $uri;
-    private array $parameters;
+    public function method(): Method;
 
-    public function __construct(Method $method, Route $route, string $uri)
-    {
-        $this->method = $method;
-        $this->route = $route;
-        $this->uri = $uri;
-    }
+    public function headers(): array;
 
-    public function method(): Method
-    {
-        return $this->method;
-    }
+    public function header(string $header): string;
 
-    public function headers(): array
-    {
-        if (isset($this->headers)) return $this->headers;
+    public function route(): Route;
 
-        foreach ($_SERVER as $key => $value) {
-            if (substr($key, 0, 5) == 'HTTP_') {
-                $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-                $this->headers[$header] = $value;
-            }
-        }
-        return $this->headers;
-    }
+    public function body(): string;
 
-    public function header(string $header): string
-    {
-        if (!array_key_exists($header, $headers = $this->headers())) throw new RequestException("header does not exist for header: ".$header);
-        return $headers[$header];
-    }
+    public function uri(): string;
 
-    public function route(): Route
-    {
-        return $this->route;
-    }
+    public function formData(): array;
 
-    public function body(): string
-    {
-        if (isset($this->body)) return $this->body;
-        return $this->body = file_get_contents('php://input');
-    }
+    public function parameters(): array;
 
-    public function uri(): string
-    {
-        return $this->uri;
-    }
-
-    public function formData(): array
-    {
-        return array_merge($_POST, $_GET);
-    }
-
-    public function parameters(): array
-    {
-        if (!isset($this->parameters)) $this->parameters = $this->route()->parameters($this->uri);
-        return $this->parameters;
-    }
-
-    public function parameter(string $key): string
-    {
-        if (!array_key_exists($key, $this->parameters())) throw new RequestException("parameter does not exist for parameter ".$key);
-        return $this->parameters[$key];
-    }
+    public function parameter(string $key): string;
 }
